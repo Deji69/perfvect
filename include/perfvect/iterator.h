93257a941f7197d6888dd93fd4fce8ffb1e52c26
Deji69 <the_zone@hotmail.co.uk>
2019-08-11 10:01:34 +0100

    Make detail::iterator fulfill LegacyInputIterator

--- include/perfvect/iterator.h
+++ include/perfvect/iterator.h
@@ -3,9 +3,12 @@
 
 #include <iterator>
 #include <type_traits>
+#include <utility>
 
 namespace perfvect {
 namespace detail {
+
+// random access iterator that must satisfy the requirements of LegacyInputIterator
 
 template<typename T>
 class iterator {
@@ -19,91 +22,105 @@ public:
 	using pointer = T*;
 	using reference = T&;
 
-	constexpr iterator() = default;
-	constexpr iterator(const iterator<std::remove_const_t<T>>& iter) : m_ptr(iter.m_ptr) {}
-	constexpr explicit iterator(pointer ptr) : m_ptr(ptr) {}
+	constexpr iterator() noexcept = default;
+	constexpr iterator(const iterator<std::remove_const_t<T>>& iter) noexcept : m_ptr(iter.m_ptr) {}
+	constexpr explicit iterator(pointer ptr) noexcept : m_ptr(ptr) {}
 
-	[[nodiscard]] constexpr auto operator*() const->reference {
+	constexpr auto& operator=(const iterator& iter) noexcept {
+		m_ptr = iter.m_ptr;
+		return *this;
+	}
+
+	[[nodiscard]] constexpr auto operator*() const noexcept->reference {
 		return *operator->();
 	}
 
-	[[nodiscard]] constexpr auto operator->() const->pointer {
+	[[nodiscard]] constexpr auto operator->() const noexcept->pointer {
 		return std::launder(m_ptr);
 	}
 
-	constexpr auto& operator++() {
+	constexpr auto& operator++() noexcept {
 		++m_ptr;
 		return *this;
 	}
 
-	constexpr auto operator++(int) {
+	constexpr auto operator++(int) noexcept {
 		auto tmp = *this;
 		++*this;
 		return tmp;
 	}
 
-	constexpr auto& operator--() {
+	constexpr auto& operator--() noexcept {
 		--m_ptr;
 		return *this;
 	}
 
-	constexpr auto operator--(int) {
+	constexpr auto operator--(int) noexcept {
 		auto tmp = *this;
 		--*this;
 		return tmp;
 	}
 
-	constexpr auto& operator+=(const difference_type off) {
+	constexpr auto& operator+=(const difference_type off) noexcept {
 		m_ptr += off;
 		return *this;
 	}
 
-	constexpr auto& operator-=(const difference_type off) {
+	constexpr auto& operator-=(const difference_type off) noexcept {
 		m_ptr -= off;
 		return *this;
 	}
 
-	[[nodiscard]] constexpr auto operator+(const difference_type off) const {
+	[[nodiscard]] constexpr auto operator+(const difference_type off) const noexcept {
 		auto tmp = *this;
 		return tmp += off;
 	}
 
-	[[nodiscard]] constexpr auto operator-(const difference_type off) const {
+	[[nodiscard]] constexpr auto operator-(const difference_type off) const noexcept {
 		auto tmp = *this;
 		return tmp -= off;
 	}
 
-	[[nodiscard]] constexpr auto operator-(const iterator& other) const {
+	[[nodiscard]] constexpr auto operator-(const iterator& other) const noexcept {
 		return static_cast<difference_type>(m_ptr - other.m_ptr);
 	}
 
-	[[nodiscard]] constexpr auto& operator[](const difference_type off) const {
+	[[nodiscard]] constexpr auto& operator[](const difference_type off) const noexcept {
 		return *(*this + off);
 	}
 
-	[[nodiscard]] constexpr auto operator==(const iterator& other) const {
+	[[nodiscard]] constexpr auto operator==(const iterator& other) const noexcept {
 		return m_ptr == other.m_ptr;
 	}
 
-	[[nodiscard]] constexpr auto operator!=(const iterator& other) const {
+	[[nodiscard]] constexpr auto operator!=(const iterator& other) const noexcept {
 		return !(*this == other);
 	}
 
-	[[nodiscard]] constexpr auto operator<(const iterator& other) const {
+	[[nodiscard]] constexpr auto operator<(const iterator& other) const noexcept {
 		return m_ptr < other.m_ptr;
 	}
 
-	[[nodiscard]] constexpr auto operator>(const iterator& other) const {
+	[[nodiscard]] constexpr auto operator>(const iterator& other) const noexcept {
 		return other < *this;
 	}
 
-	[[nodiscard]] constexpr auto operator<=(const iterator& other) const {
+	[[nodiscard]] constexpr auto operator<=(const iterator& other) const noexcept {
 		return !(other < *this);
 	}
 
+	auto swap(iterator other) {
+		std::swap(m_ptr, other.m_ptr);
+	}
+
 private:
-	pointer m_ptr;
+	pointer m_ptr = nullptr;
 };
+
+template<typename T>
+auto swap(iterator<T>& it1, iterator<T>& it2) {
+	it1.swap(it2);
+}
 
 template<typename T, typename = void>
 struct is_iterator {
@@ -120,4 +137,14 @@ constexpr bool is_iterator_v = is_iterator<T>::value;
 
 }
 }
+
+template<typename T>
+struct std::iterator_traits<perfvect::detail::iterator<T>> {
+	using difference_type = typename perfvect::detail::iterator<T>::difference_type;
+	using value_type = typename perfvect::detail::iterator<T>::value_type;
+	using pointer = typename perfvect::detail::iterator<T>::pointer;
+	using reference = typename perfvect::detail::iterator<T>::reference;
+	using iterator_category = typename perfvect::detail::iterator<T>::iterator_category;
+};
+
 #endif
